78a1948b78b40e57610d51208e5047b8ad1ec372
OverDoseOnLlamas <70150318+OverDoseOnLlamas@users.noreply.github.com>
2021-06-01 14:55:40 +0100

    Add files via upload
    adjusted mobile navbar text distance
    added slideshow to about me page for PC
    re-added layer to index page
    added welcome text to index

--- static/javascript.js
+++ static/javascript.js
@@ -1,7 +1,1 @@
-function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  $temp.remove();
-}
+
